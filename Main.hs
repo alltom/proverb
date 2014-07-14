0ee63f0cc28c70aac0b9b8caafc6ea0f2899ba4a
Tom Lieber <tom@alltom.com>
2014-07-14 01:13:39 -0700

    use foldl'

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 import Paths_proverb (getDataFileName)
 import System.Random (RandomGen, getStdGen, randomRs)
 import Data.Maybe (fromJust)
+import Data.List (foldl')
 
 main :: IO ()
 main = do
@@ -11,10 +12,8 @@ main = do
 roulette :: RandomGen g => [a] -> g -> Maybe a
 roulette [] _ = Nothing
 roulette (item:items) gen =
-	Just $ roulette' 2 item (zip items (randomRs (0, 1) gen))
+	Just $ snd $ foldl' roulette' (2, item) (zip items (randomRs (0, 1) gen))
 
-roulette' :: Int -> t -> [(t, Float)] -> t
-roulette' _ chosen [] = chosen
-roulette' depth chosen ((item, roll):rest) =
-	let chosen' = if roll < 1 / (fromIntegral depth) then item else chosen in
-	roulette' (depth + 1) chosen' rest
+roulette' :: (Int, t) -> (t, Float) -> (Int, t)
+roulette' (depth, chosen) (item, roll) =
+	(depth + 1, if roll < 1 / (fromIntegral depth) then item else chosen)
