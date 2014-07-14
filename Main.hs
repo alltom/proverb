bc15220105ab2daa47942e1f3b801404309f09ed
Tom Lieber <tom@alltom.com>
2014-07-14 00:47:49 -0700

    it works

---
+++ Main.hs
@@ -0,0 +1,22 @@
+import Paths_proverb (getDataFileName)
+import System.Random (RandomGen, getStdGen, randomRs)
+
+main :: IO ()
+main = do
+	proverbsPath <- getDataFileName "proverbs.txt"
+	input <- readFile proverbsPath
+	gen <- getStdGen
+	let (Just item) = roulette (lines input) gen
+	putStrLn item
+
+roulette :: System.Random.RandomGen g => [a] -> g -> Maybe a
+roulette [] _ = Nothing
+roulette items gen =
+	let ((item, _):rest) = zip items (randomRs (0, 1) gen) in
+	Just $ roulette' 2 item rest
+
+roulette' :: Int -> t -> [(t, Float)] -> t
+roulette' _ chosen [] = chosen
+roulette' depth chosen ((item, roll):rest) =
+	let chosen' = if roll < 1 / (fromIntegral depth) then item else chosen in
+	roulette' (depth + 1) chosen' rest
